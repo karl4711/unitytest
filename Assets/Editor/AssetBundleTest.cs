e6e0a16b3de9d7563614efa0aa695bc979d8aa51
karl4711 <karl4711@qq.com>
2016-02-16 15:39:39 +0800

    add asset bundle test.

---
+++ Assets/Editor/AssetBundleTest.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+
+public class AssetBundleTest : MonoBehaviour
+{
+
+
+    [MenuItem("AssetBundleTest/Package Bundle")]
+    public static void package()
+    {
+
+        string path = Application.streamingAssetsPath;
+        BuildPipeline.PushAssetDependencies();
+
+        BuildTarget target = BuildTarget.StandaloneWindows;
+
+        BuildPipeline.BuildAssetBundle(AssetDatabase.LoadMainAssetAtPath("Assets/Textures/0670.png"), null,
+                                       path + "/package1.assetbundle",
+                                       BuildAssetBundleOptions.CollectDependencies | BuildAssetBundleOptions.CompleteAssets
+                                       | BuildAssetBundleOptions.DeterministicAssetBundle, target);
+
+
+        BuildPipeline.BuildAssetBundle(AssetDatabase.LoadMainAssetAtPath("Assets/Materials/LogoFlash.mat"), null,
+                                       path + "/package2.assetbundle",
+                                       BuildAssetBundleOptions.CollectDependencies | BuildAssetBundleOptions.CompleteAssets
+                                       | BuildAssetBundleOptions.DeterministicAssetBundle, target);
+
+
+        BuildPipeline.PushAssetDependencies();
+        BuildPipeline.BuildAssetBundle(AssetDatabase.LoadMainAssetAtPath("Assets/Prefabs/1.prefab"), null,
+                                       path + "/package3.assetbundle",
+                                       BuildAssetBundleOptions.CollectDependencies | BuildAssetBundleOptions.CompleteAssets
+                                       | BuildAssetBundleOptions.DeterministicAssetBundle, BuildTarget.StandaloneWindows);
+        BuildPipeline.PopAssetDependencies();
+
+
+
+        BuildPipeline.PushAssetDependencies();
+        BuildPipeline.BuildAssetBundle(AssetDatabase.LoadMainAssetAtPath("Assets/Prefabs/2.prefab"), null,
+                                       path + "/package4.assetbundle",
+                                       BuildAssetBundleOptions.CollectDependencies | BuildAssetBundleOptions.CompleteAssets
+                                       | BuildAssetBundleOptions.DeterministicAssetBundle, target);
+        BuildPipeline.PopAssetDependencies();
+
+        BuildPipeline.PopAssetDependencies();
+    }
+
+
+    
+
+}
---
+++ Assets/Scripts/LoadAssetBundle.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadAssetBundle : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        unpackageTest();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+    public void unpackageTest()
+    {
+        StartCoroutine(LoadBundle("package3.assetbundle"));
+    }
+
+    IEnumerator LoadBundle(string bundlePath)
+    {
+
+        //AssetBundle.CreateFromFile()
+
+        WWW www = new WWW(string.Format("file://{0}/{1}", Application.streamingAssetsPath, bundlePath));
+        yield return www;
+
+        AssetBundle bundle = www.assetBundle;
+        Object[] objs = bundle.LoadAll();
+        foreach (Object obj in objs)
+        {
+            Debug.Log(obj.name + ": " + obj.GetType());
+        }
+
+
+        if (www.error != null)
+        {
+            Debug.LogError("Load Bundle Failed " + bundlePath + " Error Is " + www.error);
+            yield break;
+        }
+        //Do something ...
+    }
+}
